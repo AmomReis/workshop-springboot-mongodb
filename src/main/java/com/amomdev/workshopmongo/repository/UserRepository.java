5eda5954f159e3a7a10b60fe147a5ba2ab3269e6
Amom Reis <mirandoamom@gmail.com>
2025-07-22 14:56:51 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/amomdev/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.amomdev.workshopmongo.repository;
+
+import com.amomdev.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/amomdev/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.amomdev.workshopmongo.services;
+
+import com.amomdev.workshopmongo.domain.User;
+import com.amomdev.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
