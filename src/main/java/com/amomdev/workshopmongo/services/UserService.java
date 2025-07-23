a56456452730f0092ee06d7ec32c3ec13682b3a8
Amom Reis <mirandoamom@gmail.com>
2025-07-23 15:10:08 -0300

    Insercao de usuario com post

--- src/main/java/com/amomdev/workshopmongo/services/UserService.java
+++ src/main/java/com/amomdev/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.amomdev.workshopmongo.services;
 
 import com.amomdev.workshopmongo.domain.User;
+import com.amomdev.workshopmongo.dto.UserDTO;
 import com.amomdev.workshopmongo.repository.UserRepository;
 import com.amomdev.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
