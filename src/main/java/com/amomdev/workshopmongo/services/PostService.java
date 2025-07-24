239eafca441c086bc879e90f8b1c372ed0b29c76
Amom Reis <mirandoamom@gmail.com>
2025-07-24 16:03:41 -0300

    Obtendo post por Id

---
+++ src/main/java/com/amomdev/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.amomdev.workshopmongo.services;
+
+import com.amomdev.workshopmongo.domain.Post;
+import com.amomdev.workshopmongo.domain.User;
+import com.amomdev.workshopmongo.repository.PostRepository;
+import com.amomdev.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+    }
+}
