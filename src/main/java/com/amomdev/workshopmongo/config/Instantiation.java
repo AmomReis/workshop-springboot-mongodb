5f3f2083939c43d2e6d6eef85e17540562021513
Amom Reis <mirandoamom@gmail.com>
2025-07-24 16:19:36 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/amomdev/workshopmongo/config/Instantiation.java
+++ src/main/java/com/amomdev/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.amomdev.workshopmongo.config;
 import com.amomdev.workshopmongo.domain.Post;
 import com.amomdev.workshopmongo.domain.User;
 import com.amomdev.workshopmongo.dto.AuthorDTO;
+import com.amomdev.workshopmongo.dto.CommentDTO;
 import com.amomdev.workshopmongo.repository.PostRepository;
 import com.amomdev.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -35,6 +37,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, Instant.now(), "Partiu viagem!", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, Instant.now(), "Bom dia!", "Acordei feliz hoje.", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viajem!", Instant.now(), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", Instant.now(), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", Instant.now(), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2. getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
