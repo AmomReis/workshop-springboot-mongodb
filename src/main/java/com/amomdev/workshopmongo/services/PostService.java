f17a4c96d0281723d6180127a54e3075303645fe
Amom Reis <mirandoamom@gmail.com>
2025-07-24 16:41:19 -0300

    Consulta simples com query methods

--- src/main/java/com/amomdev/workshopmongo/services/PostService.java
+++ src/main/java/com/amomdev/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.amomdev.workshopmongo.services;
 
 import com.amomdev.workshopmongo.domain.Post;
-import com.amomdev.workshopmongo.domain.User;
 import com.amomdev.workshopmongo.repository.PostRepository;
 import com.amomdev.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 }
